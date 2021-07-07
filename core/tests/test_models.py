2f46ff27cdaaa5adfc148fa28c159d4dbeefcbe4
Toyin96 <onagoruwam@gmail.com>
2021-07-07 15:58:24 +0100

    completed the  user & superuser models using tdd

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,3 +1,5 @@
+from core.models import User
+from django import test
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -11,4 +13,25 @@ class TestModels(TestCase):
             email=email, 
             password=password)
         self.assertEquals(user.email, email)
-        self.assertTrue(user.check_password(password))    
+        self.assertTrue(user.check_password(password)) 
+
+    def test_new_user_email_normalize(self):
+        """test the email for a new user is normalize"""
+        email = "onagoruwam@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, "test123")
+        self.assertEquals(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test creating user with no email raises error""" 
+        with self.assertRaises(ValueError):
+            password = 'test123'
+            get_user_model().objects.create_user(email=None, password=password)
+          
+    def test_create_super_user(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            "onagoruwam@gmail.com",
+            "super123"
+        )  
+
+        self.assertTrue(User.is_superuser)
